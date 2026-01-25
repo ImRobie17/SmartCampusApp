7df787425923ec00bb6cab8c73859e37eed987f7
luluwiswiswis <taactaacangel@gmail.com>
2026-01-25 21:06:01 +0800

    chore(setup): wire app start destination and add compose nav deps

--- app/src/main/java/com/example/smartcampusapp/MainActivity.kt
+++ app/src/main/java/com/example/smartcampusapp/MainActivity.kt
@@ -4,44 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.smartcampusapp.ui.navigation.AppNav
 import com.example.smartcampusapp.ui.theme.SmartCampusAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            SmartCampusAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            // Turn off dynamic color so your UI colors match the Figma look more consistently
+            SmartCampusAppTheme(dynamicColor = false) {
+                AppNav()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartCampusAppTheme {
-        Greeting("Android")
     }
 }
